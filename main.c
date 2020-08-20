316ce358a0799e362f2d796895c367774303ec5f
cruisien <simon.chatziparaskewas@outlook.de>
2020-08-20 14:40:50 +0200

    upp down counter

--- main.c
+++ main.c
@@ -1,16 +1,37 @@
 #include <stdlib.h>
 #include <avr/io.h>
+#include <util/delay.h>
 
 // Test//
 
 int main(void)
-{
+{ DDRD = 0xFF;
+uint8_t test;
+test = 1;
+while (1){
+do{
+	test++;
+	PORTD = test;
+	_delay_ms(200);
+}while (test < 255);
+
+do{
+	test--;
+	PORTD = test;
+	_delay_ms(200);
+}while ( test > 1);	
+
+
+
+
+}
+
 	
 	
-	while(1)
-	{ 
+	
+	
 		
-	} //end while
+
 }//end of main
 
 
