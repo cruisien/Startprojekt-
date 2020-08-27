30f6f93d4893d3a1ef92383ba230db243531138d
cruisien <simon.chatziparaskewas@outlook.de>
2020-08-27 12:00:40 +0200

    LCD einzeln

--- main.c
+++ main.c
@@ -2,17 +2,15 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-// Test//
+// Test
 
 
 
 //Execute
 void LCP(void);
 
-
 //reset
 void LCR(void);
-
 
 //initialising
 void LCC(void);
@@ -20,15 +18,88 @@ void LCC(void);
 //Cursor home
 void LCH(void);
 
+//Simon
+void LCSIMON(void);
 
-int main(void)
-{ DDRD = 255;
+//output port D
+void OUTD(uint8_t BIN);
+
+
+int main(void){
+//ports enable/set high
+DDRD = 255;
 DDRC = 3;
 PORTD = 0xFF;
 PORTC = 0x3;
+//
+
+
 PORTC = 0;
 LCC();
 LCR();
+
+PORTD = 0b01000000;
+LCP();
+PORTD = 0b00001110;
+LCP();
+PORTD = 0b00010001;
+LCP();
+PORTD = 0b00001110;
+LCP();
+PORTD = 0b00000100;
+LCP();
+PORTD = 0b00011111;
+LCP();
+PORTD = 0b00000100;
+LCP();
+PORTD = 0b00001010;
+LCP();
+PORTD = 0b00010001;
+LCP();
+	
+	
+	
+
+return 0;
+}
+
+
+
+
+//execute LCD input
+void LCP(void){
+	PORTC |= (1<<PD1);
+	_delay_ms(2);
+	PORTC &= ~(1<<PD1);
+	_delay_ms(2);
+	PORTC |= (1<<PD1);
+}
+
+//clear LCD and reset cursor
+void LCR(void){
+	PORTC &= ~(1<<0);
+	PORTD = 0b00000001;
+	LCP();
+	LCC();
+}
+
+//reset cursor
+void LCH(void){
+	PORTC &= ~(1<<0);
+	PORTD = 0b00000010;
+	LCP();
+}
+
+//initialising LCD
+void LCC(void){
+	PORTC |= (1<<PD1);
+	PORTC &= ~(1<<PD0);
+	PORTD = 0b00001111;
+	LCP();
+}
+
+//Simon		
+void LCSIMON(void){
 	PORTC = 0b11;
 	PORTD = 0b01010011;
 	LCP();
@@ -40,46 +111,13 @@ LCR();
 	LCP();
 	PORTD = 0b01101110;
 	LCP();
-	LCH();
-
-
-return 0;
-}
-
-
-
-
-
-void LCP(void){
-	_delay_ms(2);
-	PORTC &= ~(1<<PD1);
-	_delay_ms(2);
-	PORTC |= (1<<PD1);
-}
-
-void LCR(void){
-	PORTC &= ~(1<<0);
-	PORTD = 0b00000001;
-	LCP();
-	LCC();
-}
-
-void LCH(void){
-	PORTC &= ~(1<<0);
-	PORTD = 0b00000010;
-	LCP();
-}
-
-void LCC(void){
-	PORTC |= (1<<PD1);
-	PORTC &= ~(1<<PD0);
-	PORTD = 0b00001111;
-	LCP();
-}
-		
-void LCA(void){
 	
-	
+}
+
+//output port D
+void OUTD(uint8_t BIN){
+	PORTD = BIN;
+	LCP();
 }
 	
 		
