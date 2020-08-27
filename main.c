79719446e41e59e59d116b29bcb24c7b7448cb6a
cruisien <simon.chatziparaskewas@outlook.de>
2020-08-27 10:43:16 +0200

    LCD Schreiben

--- main.c
+++ main.c
@@ -4,40 +4,83 @@
 
 // Test//
 
-//MODE
-void LCE(uint8_t nr){
- PORTC |= (nr<<0);
-}
+
 
 //Execute
-void LCP (void){
-	PORTC |= (1<<1);
-	PORTC |= (0<<1);
-}
+void LCP(void);
 
-//cursor
-void LCC (Void) {
-	PORTC |= (1<<0);
-	PORTD = 00001111;
-	PORTC |= (1<<1);
-	_delay_ms(100);
-	PORTC |= (0<<1);
-}
-	
-	
+
+//reset
+void LCR(void);
+
+
+//initialising
+void LCC(void);
+
+//Cursor home
+void LCH(void);
 
 
 int main(void)
-{ DDRD = 0xFF;
-DDRC |= (1<<PC0)| (1<<PC1);
-LCC;
+{ DDRD = 255;
+DDRC = 3;
+PORTD = 0xFF;
+PORTC = 0x3;
+PORTC = 0;
+LCC();
+LCR();
+	PORTC = 0b11;
+	PORTD = 0b01010011;
+	LCP();
+	PORTD = 0b01101001;
+	LCP();
+	PORTD = 0b01101101;
+	LCP();
+	PORTD = 0b01101111;
+	LCP();
+	PORTD = 0b01101110;
+	LCP();
+	LCH();
 
 
+return 0;
+}
+
+
+
+
+
+void LCP(void){
+	_delay_ms(2);
+	PORTC &= ~(1<<PD1);
+	_delay_ms(2);
+	PORTC |= (1<<PD1);
+}
+
+void LCR(void){
+	PORTC &= ~(1<<0);
+	PORTD = 0b00000001;
+	LCP();
+	LCC();
+}
+
+void LCH(void){
+	PORTC &= ~(1<<0);
+	PORTD = 0b00000010;
+	LCP();
+}
+
+void LCC(void){
+	PORTC |= (1<<PD1);
+	PORTC &= ~(1<<PD0);
+	PORTD = 0b00001111;
+	LCP();
 }
 		
-		
-
+void LCA(void){
 	
+	
+}
 	
 		
 
