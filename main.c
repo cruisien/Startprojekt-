951caac5f958fa15b80c8e1bf1218babb3991db2
cruisien <simon.chatziparaskewas@outlook.de>
2020-08-31 11:00:14 +0200

    Funkuhr Ausloesung und bit erkennung

--- main.c
+++ main.c
@@ -15,14 +15,34 @@ int main(void)
 	
 	DDRC = 0xFF;
 	PORTC = 0x00;
+	
+	//funktions variablen
 	uint8_t pegel;
 	uint8_t apegel;
 	uint16_t timecounth;
-	uint8_t timecountl;
+	uint16_t timecountl;
+	uint16_t repcount;
+	uint8_t triger;
+	uint8_t bin;
+	bin = 0;
+	triger = 0;
 	timecountl = 0;
 	timecounth = 0;
+	repcount = 0;
 	apegel = 0;
 	pegel = 0;
+	
+	//zeit variablen
+	uint8_t sek;
+	uint8_t min;
+	uint8_t hour;
+	uint16_t year;
+	sek = 0;
+	min = 0;
+	hour = 0;
+	year = 0;
+	
+	
 	
 	lcd_init(LCD_DISP_ON);  // initialisieren
 
@@ -35,34 +55,45 @@ int main(void)
 		
 		if(PIND & (1<<PD5)){
 			pegel = 1;
+			
+			if(pegel > apegel){
+			apegel = 1;
+			/*lcd_clrscr();
+			lcd_count(timecountl);*/
+			timecountl = 0;
 		}
+		}
+		
+		
+		
+		
 		else{
 			pegel = 0;
-		}
-		
-	
-		if(pegel > apegel){
-			apegel = 1;
-			/*if(timecountl > 200){
-			lcd_puts("H");
-		}*/
-		}
-		
-		                     //output daten bei negativer flanke
+			
+			                     //output daten bei negativer flanke
 		if(pegel < apegel){
 			apegel = 0;
 			/*lcd_clrscr();
 			lcd_count(timecounth);
 			timecounth = 0;*/
-			if(timecounth < 23000){
-			lcd_puts("0");
-			timecounth = 0;
+			
+			if(triger == 1){
+				sek++;
+				if(timecounth < 23000){
+				bin = 0;
+				timecounth = 0;
+				}
+				else{
+				bin = 1;
+				timecounth = 0;
+				}
+				
 			}
-		else{
-			lcd_puts("1");
-			timecounth = 0;
 		}
 		}
+		
+	
+		
 		
 		
 		
@@ -71,15 +102,20 @@ int main(void)
 		if(apegel == 1){
 			timecounth++;
 		}						//counter
-		/*if(apegel == 0){
+		else{
+			repcount++;
+			if(repcount == 8){
 			timecountl++;
-		}*/
-		
-		if(timecounth == 65500){
-			lcd_puts("overflow");
+			repcount = 0;
+			if(timecountl > 20000){
+				triger = 1;
+			}
+		}
 		}
 		
-		
+		if(timecountl == 65500){
+			lcd_puts("over");
+		}
 		
 		
 		
@@ -94,7 +130,3 @@ int main(void)
 			
 	
 	//end of main
-
-
-
-
